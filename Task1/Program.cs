a97cc2ad2be3fb56ff1c88d23dc5a10c822150fe
Julia RudenkoE <juliarudenko29@icloud.com>
2023-02-19 15:15:17 +0300

    2task

---
+++ Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите трехзначное число: ");
+    int value = Int32.Parse(Console.ReadLine());
+
+    var countSymbols = Math.Floor(Math.Log10(value) + 1);
+
+    if (countSymbols != 3)
+    {
+        Console.Write("Введите трехзначное число!");
+        return;
+    }
+
+    int result;
+    var temp = value;
+
+    do
+    {
+        result = temp % 10;
+        temp = temp / 10;
+    } while (temp > 9);
+
+    Console.WriteLine($"Вторая цифра слева числа {value} = {result}");

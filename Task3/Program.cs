fa754c28698c32d49d57e57cbae8db2fa5ac9406
Julia RudenkoE <juliarudenko29@icloud.com>
2023-02-19 15:21:45 +0300

    3Task

---
+++ Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿ Console.Write("Введите день недели (1-7): ");
+int value = Int32.Parse(Console.ReadLine());
+
+    if (value < 1 || value > 7)
+    {
+        Console.WriteLine("Введите цифру от 1 до 7");
+        return;
+    }
+
+    var isDayOff = (value == 6 || value == 7);
+    var resultStr = isDayOff ? "выходной день" : "будний день";
+    Console.WriteLine($"день недели {value} = {resultStr}");

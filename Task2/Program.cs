bbdcac9fa4640e0af5969cad6d05546c58e9b670
Julia RudenkoE <juliarudenko29@icloud.com>
2023-02-19 15:18:49 +0300

    2Task

---
+++ Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿    Console.Write("Введите число: ");
+    int value = Int32.Parse(Console.ReadLine());
+
+    var countSymbols = Math.Floor(Math.Log10(value) + 1);
+
+    if (countSymbols < 3)
+    {
+        Console.WriteLine("Минимум 3 символа!");
+        return;
+    }
+
+    int result;
+    var temp = value;
+
+    do
+    {
+        result = temp % 10;
+        temp = temp / 10;
+    } while (temp > 99);
+
+    Console.WriteLine($"Третья цифра слева числа {value} = {result}");
